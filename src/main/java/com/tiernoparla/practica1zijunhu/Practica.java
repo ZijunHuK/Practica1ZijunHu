53bd50daf49f6ba6680c5ed1addf44e2e23d16da
Zijun Hu <zijun.hu@tiernoparla.es>
2022-11-23 21:52:06 +0100

    avanzado_algo

--- src/main/java/com/tiernoparla/practica1zijunhu/Practica.java
+++ src/main/java/com/tiernoparla/practica1zijunhu/Practica.java
@@ -11,34 +11,37 @@ public class Practica {
     //introducir número
     Scanner sca = new Scanner(System.in);
 
-    System.out.println("Introduce tu numero que quiere: ");
+    System.out.println("Introduce cantidad de tu mensaje: ");
     
     int num = sca.nextInt();
 
-    System.out.println("numero que tengo son: " + num);
+    System.out.println("cantidad que tengo son: " + num);
     
     //generar el número al 0 y 1
     Random ram = new Random();
     
     for (int i = 0; i < num; i++) {
-      int dado = ram.nextInt(2);
-      System.out.print(dado + " ");
+      int binario = ram.nextInt(2);
+      System.out.print(binario + " ");
     }
-    
         System.out.println(" ");
-    
-     
+
     //PARTE 2
+    
+    int elevado = (int) Math.pow(2, 3)-(3+1);
+    int elevado1 = (int) Math.pow(2, 4)-(4+1);
+    if (elevado < num && num > elevado1) {
+        System.out.println("true");
+    }
+    /*
     for (int j = 1; j < num; j++) {
         int elevado = (int) Math.pow(2, j)-(j+1);
         System.out.println("2elevado a " + j + " = " + elevado);
-        for (int k = 0; k >= num; k++ ) {
-            System.out.println("porfa" + k);
+        if (num < elevado){
+            System.out.println("true");   
+        }
     }
-    }
-    
-    
-        
+    */ 
         
         
         
